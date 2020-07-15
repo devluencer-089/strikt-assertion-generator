7de83e40e67ba7a8a98ed36e71a8e26ba54c90ef
Michael Omann <michael.omann@gmail.com>
2020-07-15 22:43:24 +0200

    make test consistent

--- examples/src/test/kotlin/com/michaelom/strikt/generator/example/ExpressionSpec.kt
+++ examples/src/test/kotlin/com/michaelom/strikt/generator/example/ExpressionSpec.kt
@@ -9,17 +9,37 @@ class ExpressionSpec : Spek({
 
     describe("generated assertions for Expression.kt") {
 
-        it("generates lazy assertions") {
+        context("without generated assertions") {
 
-            expectThat(Const(number = 1.0)) {
-                number isEqualTo 1.0
+            it("looks beautify") {
+
+                expectThat(Const(number = 1.0)) {
+                    get { number } isEqualTo 1.0
+                }
+
+                val sum = Sum(e1 = Const(1.0), e2 = Const(1.0))
+
+                expectThat(sum) {
+                    get { e1 } isEqualTo Const(1.0)
+                    get { e2 } isEqualTo Const(1.0)
+                }
             }
+        }
 
-            val sum = Sum(e1 = Const(1.0), e2 = Const(1.0))
+        context("with generated assertions") {
 
-            expectThat(sum) {
-                e1 isEqualTo Const(1.0)
-                e2 isEqualTo Const(1.0)
+            it("looks beautify") {
+
+                expectThat(Const(number = 1.0)) {
+                    number isEqualTo 1.0
+                }
+
+                val sum = Sum(e1 = Const(1.0), e2 = Const(1.0))
+
+                expectThat(sum) {
+                    e1 isEqualTo Const(1.0)
+                    e2 isEqualTo Const(1.0)
+                }
             }
         }
     }
